f0690b7486ee0e609204cd858172066679cc2d10
joselora <jsloravargas@gmail.com>
2025-02-03 22:12:57 -0500

    25 Changes 02-03-2025

--- php/smtp_test.php
+++ php/smtp_test.php
@@ -1,6 +1,7 @@
 <?php
 $host = 'smtp.gmail.com';
-$port = 465; // Puedes probar también con 465 o 25
+$port = 25; // Puedes probar también con 465 o 25
+
 
 echo "Probando conexión SMTP a $host:$port...<br>";
 
